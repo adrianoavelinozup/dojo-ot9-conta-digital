c87450b39a86cfb69c84c460e4f5e428f239689f
Adriano Avelino <adriano.avelino@zup.com.br>
2021-12-06 18:40:31 -0300

    inicia controle de versão

---
+++ src/main/java/br/com/zup/academy/contadigital/contadigital/ContadigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.contadigital.contadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadigitalApplication.class, args);
+	}
+
+}
